29f6f7de43aded19fed6e53373b35d02d946703c
アリエル <parrabarria@hotmail.com>
2020-05-20 11:23:55 +0900

    Add files via upload

---
+++ MLKF/training/word2vec_model_train.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer, WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from gensim.models.doc2vec import TaggedDocument
+from gensim.models.word2vec import Word2Vec
+from gensim.models import phrases
+
+df = pd.read_csv("dataset",header=1,names=["A","B","C","D"])
+
+##########################use same preprocessing as specific dataset#######################################################################################################
+
+bigrams = phrases.Phrases(sentences)
+
+epoch_list = [50]
+size_list = [10]
+
+for x in epoch_list:
+	for y in size_list:
+
+		vec_size = y
+		max_epochs = x
+		mode = 0
+		model = Word2Vec(size=vec_size, sg=mode, iter=max_epochs)
+		model.build_vocab(bigrams[sentences])
+		print (model.build_vocab(bigrams[sentences]))
